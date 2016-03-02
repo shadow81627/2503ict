3fdce333d6bdcb4163dfc51a8c18e597e8ef508c
shadow81627 <s5014219@griffithuni.edu.au>
2016-03-02 06:04:45 +0000

    lab1

--- lab1/index.php
+++ lab1/index.php
@@ -7,6 +7,31 @@
         <meta name="viewport" content="width=device-width">
     </head>
     <body>
-        <h1>Hello World</h1>
+        <h1>A Heading</h1>
+        <ol>
+            <li>things</li>
+            <li>things</li>
+            <li>things</li>
+        </ol>
+        <table>
+            <tr>
+                <td>things</td>
+                <td>things</td>
+                <td>things</td>
+            </tr>
+            <tr>
+                <td>things</td>
+                <td>things</td>
+                <td>things</td>
+            </tr>
+        </table>
+        <img src="https://upload.wikimedia.org/wikipedia/en/2/2a/Griffith_University_logo.png"/>
+        <address>
+    		<ul class="list-style">
+    			<li><span class ="bold">Email: </span><a href="mailto:s5014219@griffithuni.com">s5014219@griffithuni.com</a></li>
+    			<li><span class ="bold">Phone: </span>(07) 123456789</li>
+    		</ul>
+		</address>
+		<a href="page2.html">PAGE2</a>
     </body>
 </html> 
