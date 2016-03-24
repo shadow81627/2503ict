c2765f5e5cd0337ba0da2e4cc16f5a429c2713a5
Damien Robinson <damien.robinson@griffithuni.edu.au>
2016-03-24 02:03:23 +0000

    added lab4

---
+++ lab4/assoc-laravel/app/routes.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+/* Load sample data, an array of associative arrays. */
+require "models/pms.php";
+
+
+// Display search form
+Route::get('/', function()
+{
+	return View::make('pms.query');
+});
+
+// Perform search and display results
+Route::get('search', function()
+{
+  $name = Input::get('name');
+  $year = Input::get('year');
+  $state = Input::get('state');
+
+  $results = search($name, $year, $state);
+
+	return View::make('pms.results')->withPms($results);
+});
+
+
+/* Functions for PM database example. */
+
+/* Search sample data for $name or $year or $state from form. */
+function search($search) {
+    global $pms; 
+	
+	if(!empty($search)){
+		$words = explode ( " " , $search);
+		foreach($words as $word){
+			 // Filter $pms by $search
+			$results = array();
+			foreach ($pms as $pm) {
+			    if (stripos($pm['name'], $word) !== FALSE ||
+			    	strpos($pm['address'], $word) !== FALSE || 
+			        strpos($pm['phone'], $word) !== FALSE ||
+			        stripos($pm['email'], $word) !== FALSE){
+			        $results[] = $pm;
+				}
+			}
+		}
+		$pms = $results;
+	}
+	return $pms;
+}
+?>
