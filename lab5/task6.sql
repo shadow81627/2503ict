92a67695cb9411cf04058643a251a584a1197d2a
Damien Robinson <damien.robinson@griffithuni.edu.au>
2016-04-11 04:07:05 +0000

    completed task 6

---
+++ lab5/task6.sql
@@ -0,0 +1,13 @@
+/*
+* returns the names of customers who live in Nathan
+*/
+SELECT c.NAME FROM CUSTOMERS AS c
+WHERE c.ADDRESS LIKE "%Nathan%"; 
+
+/*
+* returns the names of customers who have bought Fred's Fries
+*/
+SELECT c.NAME FROM CUSTOMERS AS c
+WHERE c.ID IN(SELECT o.CUSTID FROM ORDERS AS o
+    WHERE o.ITEMID IN(SELECT s.ID FROM STOCK AS s
+        WHERE s.NAME = "Fred's Fries"));
