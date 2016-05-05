0bc40756a8b658c8735346968734a63f19809e26
Damien Robinson <damien.robinson@griffithuni.edu.au>
2016-05-05 04:50:00 +0000

    added product and product seeder

--- lab8/products/app/database/seeds/DatabaseSeeder.php
+++ lab8/products/app/database/seeds/DatabaseSeeder.php
@@ -1,17 +1,8 @@
 <?php
 
 class DatabaseSeeder extends Seeder {
-
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Eloquent::unguard();
-
-		// $this->call('UserTableSeeder');
-	}
-
+	public function run() {
+	 	Eloquent::unguard();
+		$this->call('ProductsTableSeeder');
+ }
 }
