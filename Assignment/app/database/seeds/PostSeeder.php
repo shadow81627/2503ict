711162ceaa5de6b01e7efe38e223070de261013d
Damien Robinson <damien.robinson@griffithuni.edu.au>
2016-05-27 00:55:34 +0000

    added posts by tom

--- Assignment/app/database/seeds/PostSeeder.php
+++ Assignment/app/database/seeds/PostSeeder.php
@@ -3,6 +3,7 @@ class PostSeeder extends Seeder {
     public function run() {
         Eloquent::unguard();
         
+        //Posts by Bob
         $post = new Post;
         $post->title = 'Bob\'s Post 1';
         $post->icon = 'https://upload.wikimedia.org/wikipedia/en/2/2a/Griffith_University_logo.png';
@@ -25,6 +26,87 @@ class PostSeeder extends Seeder {
         $post->message = 'Bob\'s private post';
         $post->post_name = 'Bob';
         $post->privacy = 'Private';
+        $post->save();
+        
+        //Posts by Tom
+        $post = new Post;
+        $post->title = 'Tom\'s Post 1';
+        $post->icon = 'https://upload.wikimedia.org/wikipedia/en/2/2a/Griffith_University_logo.png';
+        $post->message = 'Tom\'s public post ';
+        $post->post_name = 'Tom';
+        $post->privacy = 'Public';
+        $post->save();
+        
+        $post = new Post;
+        $post->title = 'Tom\'s Post 2';
+        $post->icon = 'https://pbs.twimg.com/profile_images/378800000003051007/01e9f11c2c487fe75f5ae6e1fd3e1d33_400x400.jpeg';
+        $post->message = 'Bob\'s friends post';
+        $post->post_name = 'Tom';
+        $post->privacy = 'Friends';
+        $post->save();
+        
+        $post = new Post;
+        $post->title = 'Tom\'s Post 3';
+        $post->icon = 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Big_Bear_Valley,_California.jpg';
+        $post->message = 'Tom\'s private post';
+        $post->post_name = 'Tom';
+        $post->privacy = 'Private';
+        $post->save();
+        
+        $post = new Post;
+        $post->title = 'Tom\'s Post 4';
+        $post->icon = 'https://upload.wikimedia.org/wikipedia/en/2/2a/Griffith_University_logo.png';
+        $post->message = 'Tom\'s public post ';
+        $post->post_name = 'Tom';
+        $post->privacy = 'Public';
+        $post->save();
+        
+        $post = new Post;
+        $post->title = 'Tom\'s Post 5';
+        $post->icon = 'https://pbs.twimg.com/profile_images/378800000003051007/01e9f11c2c487fe75f5ae6e1fd3e1d33_400x400.jpeg';
+        $post->message = 'Tom\'s friends post';
+        $post->post_name = 'Tom';
+        $post->privacy = 'Friends';
+        $post->save();
+        
+        $post = new Post;
+        $post->title = 'Tom\'s Post 6';
+        $post->icon = 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Big_Bear_Valley,_California.jpg';
+        $post->message = 'Tom\'s private post';
+        $post->post_name = 'Tom';
+        $post->privacy = 'Private';
+        $post->save();
+        
+        $post = new Post;
+        $post->title = 'Tom\'s Post 7';
+        $post->icon = 'https://upload.wikimedia.org/wikipedia/en/2/2a/Griffith_University_logo.png';
+        $post->message = 'Tom\'s public post ';
+        $post->post_name = 'Tom';
+        $post->privacy = 'Public';
+        $post->save();
+        
+        $post = new Post;
+        $post->title = 'Tom\'s Post 8';
+        $post->icon = 'https://pbs.twimg.com/profile_images/378800000003051007/01e9f11c2c487fe75f5ae6e1fd3e1d33_400x400.jpeg';
+        $post->message = 'Tom\'s friends post';
+        $post->post_name = 'Tom';
+        $post->privacy = 'Friends';
+        $post->save();
+        
+        $post = new Post;
+        $post->title = 'Tom\'s Post 9';
+        $post->icon = 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Big_Bear_Valley,_California.jpg';
+        $post->message = 'Tom\'s private post';
+        $post->post_name = 'Tom';
+        $post->privacy = 'Private';
+        $post->save();
+        
+        $post = new Post;
+        $post->title = 'Tom\'s Post 10';
+        $post->icon = 'https://upload.wikimedia.org/wikipedia/en/2/2a/Griffith_University_logo.png';
+        $post->message = 'Tom\'s public post ';
+        $post->post_name = 'Tom';
+        $post->privacy = 'Public';
         $post->save();
     }
 }
